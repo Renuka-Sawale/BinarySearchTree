4e45a079883de52296d910c0290c37bbcf68ff51
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-20 20:46:24 +0530

    Searching 63 node in Binary Search Tree

--- src/main/java/com/binarysearchtree/MyBinaryTree.java
+++ src/main/java/com/binarysearchtree/MyBinaryTree.java
@@ -29,5 +29,32 @@ public class MyBinaryTree<K extends Comparable<K>> {
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
                                        + this.getSizeRecursive(current.right);
     }
+
+    public void traverseInOrder(MyBinaryNode node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.key);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void print() {
+        traverseInOrder(root);
+    }
+
+    public boolean search(K key) {
+        return searchRecursive(root, key);
+    }
+
+    private boolean searchRecursive(MyBinaryNode<K> current, K key) {
+        if (current == null)
+            return false;
+        else if (current.key.compareTo(key) == 0)
+            return true;
+        else if (current.key.compareTo(key) < 0)
+            return searchRecursive(current.right, key);
+        else
+            return searchRecursive(current.left, key);
+    }
 }
 
